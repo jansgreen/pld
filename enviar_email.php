6da313ce4020fd80bb124013cfe0cc2ae7a7ac6c
jansgreen <jansgreen@gmail.com>
2023-02-23 15:34:39 -0500

    actualizado

--- enviar_email.php
+++ enviar_email.php
@@ -1,13 +1,19 @@
 <?php
   // Recibe los datos del formulario
-  $nombre = $_POST['nombre'];
+  $nombre = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $cedula = $_POST['cedula'];
   $email = $_POST['email'];
-  $mensaje = $_POST['mensaje'];
+  $address = $_POST['address'];
+  $provincia = $_POST['provincia'];
+  $municipios = $_POST['municipios'];
+  $zip = $_POST['zip'];
+  $Empadronador = $_POST['Empadronador'];
   
   // Configuración del correo electrónico
   $destinatario = 'jansgreen@example.com';
-  $asunto = 'Mensaje de formulario de contacto';
-  $cuerpo = "Nombre: $nombre\nEmail: $email\nMensaje: $mensaje";
+  $asunto = "Aqui te envio mis datos para registrarme" + [$nombre, $lastName,  $cedula, $email,  $address, $provincia, $municipios,  $zip,  $Empadronador ];
+  $cuerpo = "Nombre: $nombre, $lastName \n Email: $email\nMensaje: $mensaje";
   $headers = "From: $email\r\nReply-To: $email\r\n";
   
   // Envía el correo electrónico
