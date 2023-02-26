b2cf34530536fb85289024048d0f787a5a00d0c1
jansgreen <jansgreen@gmail.com>
2023-02-26 11:36:00 -0500

    trabajando en el php

--- enviar_email.php
+++ enviar_email.php
@@ -1,4 +1,9 @@
 <?php
+
+
+  if (isset($_POST['enviar'])) {
+    if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['cedula']) && !empty($_POST['email']) 
+    && !empty($_POST['address']) && !empty($_POST['provincia']) && !empty($_POST['municipios']) && !empty($_POST['zip']) && !empty($_POST['Empadronador']) ) {
   // Recibe los datos del formulario
   $nombre = $_POST['firstName'];
   $lastName = $_POST['lastName'];
@@ -11,15 +16,15 @@
   $Empadronador = $_POST['Empadronador'];
   
   // Configuración del correo electrónico
-  $destinatario = 'jansgreen@example.com';
+  $destinatario = 'jansgreen@gmail.com';
   $asunto = "Aqui te envio mis datos para registrarme" + [$nombre, $lastName,  $cedula, $email,  $address, $provincia, $municipios,  $zip,  $Empadronador ];
   $cuerpo = "Nombre: $nombre, $lastName \n Email: $email\nMensaje: $mensaje";
-  $headers = "From: $email\r\nReply-To: $email\r\n";
+  $headers = "From: $email\r\nReply-To: $email\r\n";    }else {
+      echo 'Error al enviar el mensaje';
+    } 
+  }else {
+    echo 'no se registro el evento';
+  } 
   
-  // Envía el correo electrónico
-  if (mail($destinatario, $asunto, $cuerpo, $headers)) {
-    echo 'Mensaje enviado exitosamente';
-  } else {
-    echo 'Error al enviar el mensaje';
-  }
+
 ?>
